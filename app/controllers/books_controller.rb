a672b517d9127b444c848caf04d5c592fb3acaf8
EC2 Default User <ec2-user@ip-172-31-46-193.ec2.internal>
2020-12-02 12:29:20 +0000

    Finish

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,59 @@
+class BooksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :ensure_correct_book, only: [:update, :edit]
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:success] = "You have creatad book successfully."
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render 'index'
+    end
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:success] = "You have update book successfully."
+      redirect_to book_path(@book)
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+
+  def ensure_correct_book
+    @book = Book.find(params[:id])
+    unless @book.user.id == current_user.id
+      redirect_to books_path
+    end
+  end
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+end
